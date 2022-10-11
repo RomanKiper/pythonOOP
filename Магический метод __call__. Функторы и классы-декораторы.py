8a7cb2ef4e830e5615e5dea1574c2076e4ff7e0f
Kiper Roman <roman_kiper@mail.ru>
2022-10-12 00:02:04 +0300

    Commit 121022

---
+++ Магический метод __call__. Функторы и классы-декораторы.py
@@ -0,0 +1,20 @@
+# class Counter:
+#     def __init__(self):\
+#         self.__counter = 0
+#
+#     def __call__(self, step =1,  *args, **kwargs):
+#         print("__call__")
+#         self.__counter += step
+#         return self.__counter
+#
+# c = Counter()
+# c2 = Counter()
+# c(5)
+# c(2)
+# c(3)
+# c2()
+# c2()
+# res = c(3)
+# res2 = c2(8)
+# print(res, res2)
+
